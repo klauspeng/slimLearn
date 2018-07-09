9f6c7bcdcc800c3e2c8522d8015d7fca72057ef4
klaus <2511634167@qq.com>
2018-07-10 06:11:22 +0800

    忽略文件

---
+++ application/common/config/System.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Project :slimLearn
+ * User: Klaus
+ * Date: 2018.05.21 6:25
+ */
+
+namespace Config;
+
+
+class System
+{
+
+}
--- public/index.php
+++ public/index.php
@@ -1,10 +1,19 @@
 <?php
-define('ROOT_PATH',dirname(__DIR__).DIRECTORY_SEPARATOR);
+define('ROOT_PATH', dirname(__DIR__) . DIRECTORY_SEPARATOR);
+define('APP_PATH', ROOT_PATH . 'application' . DIRECTORY_SEPARATOR);
+define('COMMON_PATH', APP_PATH . 'common' . DIRECTORY_SEPARATOR);
+define('ADMIN_PATH', APP_PATH . 'admin' . DIRECTORY_SEPARATOR);
+define('HOME_PATH', APP_PATH . 'home' . DIRECTORY_SEPARATOR);
 
-require ROOT_PATH.'vendor/autoload.php';
+require ROOT_PATH . 'vendor/autoload.php';
 
-function debug($content){
+function debug($content)
+{
     \ChromePhp::info($content);
 }
+
 $app = new \Slim\Slim();
+
+var_dump($app->request());exit();
+
 $app->run();
